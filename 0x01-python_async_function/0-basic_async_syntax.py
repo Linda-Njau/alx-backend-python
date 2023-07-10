5bb6ee01aad4178c9de5de84e6ac6fddf1425f25
Linda-Njau <lindanjau21@gmail.com>
2023-07-10 22:17:55 +0300

    pycodstyling for task 0 & 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,8 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """wait_random func that takes max_delay default of 10"""
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,9 +7,9 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int=10) -> List[float]:
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Waits for async function n times"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delay = [await task for task in asyncio.as_completed(tasks)]
     return delay
- 
